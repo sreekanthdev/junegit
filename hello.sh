69c9065e0b359781fc7d1b637393184a7e4762ed
sreekanth <ubuntu@ip-172-31-25-52.ap-southeast-1.compute.internal>
2019-06-14 06:32:20 +0000

    c

---
+++ hello.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#this comment section
+echo "hello this is sreekanth world" #this also comment
+echo the shell name is  $BASH
+echo the version of bash is $BASH_VERSION
+echo the bash home is $HOME
+echo the present working directory is $PWD
+Name=Mark
+val=10
+echo the name is $Name
+echo the value is $val
+
+
+echo "enter names:"  
+read name
+echo "Enter the Names:" $name
---
+++ hello1.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo this read command using scripting
+echo "Enter the names : "
+read name1 name2 name3
+echo "Enter the name of the : $name"
+echo this is for printing and read values form keyborad
+echo "Enter the vals : "
+read value1 value2 value3
+echo "Enter the value : $value"
+
